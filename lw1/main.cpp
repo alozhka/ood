c2e273cacf7603992c3459838374f6685faf741a
Sergey Lozhkin <alozhka@yahoo.com>
2025-09-11 19:46:47 +0300

    feat: добавил костяк проекта

---
+++ lw1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Добро пожаловать в SimUDuck!" << std::endl;
+    std::cout << "Проект успешно собран с помощью CMake." << std::endl;
+    
+    return 0;
+}

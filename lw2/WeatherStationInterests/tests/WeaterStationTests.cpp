ac192e9561d0f551a7dd44b13d2b0974ccb47d4f
Sergey Lozhkin <alozhka@yahoo.com>
2025-10-13 22:08:39 +0300

    tests: написал тесты для наблюдателя по событиям

--- lw2/WeatherStationInterests/tests/WeaterStationTests.cpp
+++ lw2/WeatherStationInterests/tests/WeaterStationTests.cpp
@@ -1,82 +1,122 @@
-#include "../src/Display.h"
-#include "MockDisplay.h"
+#include "../src/WeatherData.h"
+#include "../src/AnimalWorld.h"
 
 #include "gtest/gtest.h"
+#include <vector>
+
+class CountingObserver : public IObserver<WeatherInfo, WeatherType>
+{
+public:
+	int GetCallCount() const { return m_count; }
+	WeatherType GetLastEvent() const { return m_lastEvent; }
+
+private:
+	void Update(const WeatherInfo& data, const WeatherType& eventType) override
+	{
+		++m_count;
+		m_lastEvent = eventType;
+	}
+
+	int m_count = 0;
+	WeatherType m_lastEvent{};
+};
+
+class OrderTrackingObserver : public IObserver<WeatherInfo, WeatherType>
+{
+public:
+	OrderTrackingObserver(std::vector<int>& order, int id)
+		: m_order(order), m_id(id) {}
+
+private:
+	void Update(const WeatherInfo& data, const WeatherType& eventType) override
+	{
+		m_order.push_back(m_id);
+	}
+
+	std::vector<int>& m_order;
+	int m_id;
+};
 
 class WeatherStationTests : public testing::Test
 {
 protected:
-	WeatherData indoor{};
-	WeatherData outdoor{};
+	WeatherData weatherData{};
 };
 
-TEST_F(WeatherStationTests, ObserverSafelyUnsubscibesItself)
+TEST_F(WeatherStationTests, ObserverReceivesOnlySubscribedEvent)
 {
-	BrokenDisplay display{ indoor };
-	indoor.RegisterObserver(display, 1);
+	CountingObserver observer;
+	weatherData.RegisterObserver(observer, 1, WeatherType::Temperature);
 
-	indoor.SetMeasurements(1, 1, 1, 10, 30);
-	indoor.SetMeasurements(1, 1, 1, 10, 30);
+	weatherData.SetTemperature(25.0);
+	EXPECT_EQ(1, observer.GetCallCount());
+	EXPECT_EQ(WeatherType::Temperature, observer.GetLastEvent());
 
-	EXPECT_EQ(1, display.GetCallCount());
+	weatherData.SetHumidity(60.0);
+	EXPECT_EQ(1, observer.GetCallCount());
 }
 
-TEST_F(WeatherStationTests, ObserversAreNotifiedByPriority)
+TEST_F(WeatherStationTests, ObserverCanSubscribeToMultipleEvents)
+{
+	CountingObserver observer;
+	weatherData.RegisterObserver(observer, 1, WeatherType::Temperature);
+	weatherData.RegisterObserver(observer, 1, WeatherType::Pressure);
+
+	weatherData.SetTemperature(25.0);
+	EXPECT_EQ(1, observer.GetCallCount());
+
+	weatherData.SetPressure(760.0);
+	EXPECT_EQ(2, observer.GetCallCount());
+
+	weatherData.SetHumidity(60.0);
+	EXPECT_EQ(2, observer.GetCallCount());
+}
+
+TEST_F(WeatherStationTests, ObserversNotifiedByPriority)
 {
 	std::vector<int> callOrder;
+	OrderTrackingObserver obs1(callOrder, 1);
+	OrderTrackingObserver obs2(callOrder, 2);
+	OrderTrackingObserver obs3(callOrder, 3);
 
-	OrderTestDisplay display1(callOrder, 1);
-	OrderTestDisplay display2(callOrder, 2);
-	OrderTestDisplay display3(callOrder, 3);
-	OrderTestDisplay display4(callOrder, 4);
+	weatherData.RegisterObserver(obs1, 5, WeatherType::Temperature);
+	weatherData.RegisterObserver(obs2, 10, WeatherType::Temperature);
+	weatherData.RegisterObserver(obs3, 3, WeatherType::Temperature);
 
-	indoor.RegisterObserver(display1, 5);
-	indoor.RegisterObserver(display3, 10);
-	indoor.RegisterObserver(display2, 10);
-	indoor.RegisterObserver(display4, 3);
+	weatherData.SetTemperature(25.0);
 
-	indoor.SetMeasurements(1, 1, 1, 10, 30);
-
-	ASSERT_EQ(4u, callOrder.size());
-	EXPECT_EQ(callOrder[0], 3);
-	EXPECT_EQ(callOrder[1], 2);
-	EXPECT_EQ(callOrder[2], 1);
-	EXPECT_EQ(callOrder[3], 4);
+	ASSERT_EQ(3u, callOrder.size());
+	EXPECT_EQ(2, callOrder[0]); // Приоритет 10
+	EXPECT_EQ(1, callOrder[1]); // Приоритет 5
+	EXPECT_EQ(3, callOrder[2]); // Приоритет 3
 }
 
-TEST_F(WeatherStationTests, DuplicateRegistrationIsIgnored)
+TEST_F(WeatherStationTests, UnsubscribeWorks)
 {
-	WeatherData data;
-	std::vector<int> callOrder;
+	CountingObserver observer;
+	weatherData.RegisterObserver(observer, 1, WeatherType::Temperature);
 
-	OrderTestDisplay display(callOrder, 1);
+	weatherData.SetTemperature(25.0);
+	EXPECT_EQ(1, observer.GetCallCount());
 
-	data.RegisterObserver(display, 5);
-	data.RegisterObserver(display, 10);
+	weatherData.RemoveObserver(observer, WeatherType::Temperature);
 
-	data.SetMeasurements(1, 1, 1, 10, 30);
-
-	ASSERT_EQ(1u, callOrder.size());
+	weatherData.SetTemperature(30.0);
+	EXPECT_EQ(1, observer.GetCallCount());
 }
 
-TEST_F(WeatherStationTests, RemovingNonExistentObserverIsIgnored)
+TEST_F(WeatherStationTests, DuplicateRegistrationIgnored)
 {
-	BrokenDisplay display(indoor);
+	CountingObserver observer;
+	weatherData.RegisterObserver(observer, 1, WeatherType::Temperature);
+	weatherData.RegisterObserver(observer, 5, WeatherType::Temperature);
 
-	indoor.RemoveObserver(display);
-
-	EXPECT_NO_THROW(indoor.RemoveObserver(display));
+	weatherData.SetTemperature(25.0);
+	EXPECT_EQ(1, observer.GetCallCount());
 }
 
-TEST_F(WeatherStationTests, DisplayGetsDataFromBothSensors)
+TEST_F(WeatherStationTests, RemoveNonExistentObserverNoThrow)
 {
-	MockDisplay display(indoor, outdoor);
-
-	indoor.SetMeasurements(1, 1, 1, 10, 30);
-
-	EXPECT_EQ(1, display.GetCallCount());
-
-	outdoor.SetMeasurements(1, 1, 1, 10, 30);
-
-	EXPECT_EQ(2, display.GetCallCount());
+	CountingObserver observer;
+	EXPECT_NO_THROW(weatherData.RemoveObserver(observer, WeatherType::Temperature));
 }

5d32276ee40f0ad7a0099959b377b4251307f82b
Sergey Lozhkin <alozhka@yahoo.com>
2025-10-31 02:59:23 +0300

    feat: сделал робота

---
+++ lw5/Robot/main.cpp
@@ -0,0 +1,76 @@
+#include "src/Menu.h"
+#include "src/Robot.h"
+
+#include <iostream>
+
+void BeginMacroCommand(Menu& menu)
+{
+	std::string shortcut, description;
+	std::vector<std::string> commands;
+
+	std::cout << "Command name: ";
+	do
+	{
+		std::getline(std::cin, shortcut);
+	} while (menu.IsCommandExists(shortcut) && std::cout << "Command already exists!\n");
+
+	std::cout << "Command description: ";
+	std::getline(std::cin, description);
+
+	std::cout << "Write your commands. Type `end_macro` to finish recording a macro command\n";
+
+	std::string command;
+	while (std::cout << ">"
+		&& std::getline(std::cin, command)
+		&& command != "end_macro")
+	{
+		if (menu.IsCommandExists(command))
+		{
+			commands.push_back(command);
+		}
+		else
+		{
+			std::cout << "Command does not exist!\n";
+		}
+	}
+
+	menu.AddMacroCommand(shortcut, description, commands);
+	std::cout << "Macro command is saved\n";
+}
+
+void AddCommands(Menu& menu, Robot& robot)
+{
+	menu.AddItem("on", "Turns the Robot on", [&] {
+		robot.TurnOn();
+	});
+	menu.AddItem("off", "Turns the Robot off",
+		std::bind_front(&Robot::TurnOff, &robot));
+	menu.AddItem("north", "Makes the Robot walk north",
+		std::bind_front(&Robot::Walk, &robot, WalkDirection::North));
+	menu.AddItem("south", "Makes the Robot walk south",
+		std::bind_front(&Robot::Walk, &robot, WalkDirection::South));
+	menu.AddItem("west", "Makes the Robot walk west",
+		std::bind_front(&Robot::Walk, &robot, WalkDirection::West));
+	menu.AddItem("east", "Makes the Robot walk east",
+		std::bind_front(&Robot::Walk, &robot, WalkDirection::East));
+	menu.AddItem("stop", "Stops the Robot",
+		std::bind_front(&Robot::Stop, &robot));
+	menu.AddItem("begin_macro", "Begins recording macro command", [&] {
+		BeginMacroCommand(menu);
+	});
+	menu.AddItem("help", "Show instructions",
+		std::bind_front(&Menu::ShowInstructions, &menu));
+	menu.AddItem("exit", "Exit from this menu",
+		std::bind_front(&Menu::Exit, &menu));
+}
+
+int main()
+{
+	Menu menu;
+	Robot robot;
+	AddCommands(menu, robot);
+
+	menu.Run();
+
+	return 0;
+}
---
+++ lw5/Robot/src/Menu.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Menu
+{
+public:
+	using Command = std::function<void()>;
+	void AddItem(
+		const std::string& shortcut,
+		const std::string& description,
+		const Command& command)
+	{
+		m_items.emplace_back(shortcut, description, command);
+	}
+
+	void AddMacroCommand(
+		const std::string& shortcut,
+		const std::string& description,
+		const std::vector<std::string>& shortcuts)
+	{
+		std::vector<Command> commands;
+		for (const std::string& command : shortcuts)
+		{
+			const auto it = std::ranges::find_if(m_items, [&](const Item& item) {
+				return item.shortcut == command;
+			});
+			if (it != m_items.end())
+			{
+				commands.push_back(it->command);
+			}
+		}
+
+		Command macroCommand = [commands] {
+			for (const auto& command : commands)
+			{
+				command();
+			}
+		};
+		m_items.emplace_back(shortcut, description, macroCommand);
+	}
+
+	void Run()
+	{
+		ShowInstructions();
+
+		std::string command;
+		while (std::cout << ">"
+			&& getline(std::cin, command)
+			&& ExecuteCommand(command))
+		{
+		}
+	}
+
+	void ShowInstructions() const
+	{
+		std::cout << "Commands list:\n";
+		for (auto& item : m_items)
+		{
+			std::cout << "  " << item.shortcut << ": " << item.description << "\n";
+		}
+	}
+
+	void Exit()
+	{
+		m_exit = true;
+	}
+
+	bool IsCommandExists(const std::string& shortcut)
+	{
+		const auto it = std::ranges::find_if(m_items, [&](const Item& item) {
+			return item.shortcut == shortcut;
+		});
+		return it != m_items.end();
+	}
+
+private:
+	bool ExecuteCommand(const std::string& command)
+	{
+		m_exit = false;
+		auto it = std::ranges::find_if(m_items, [&](const Item& item) {
+			return item.shortcut == command;
+		});
+		if (it != m_items.end())
+		{
+			it->command();
+		}
+		else
+		{
+			std::cout << "Unknown command\n";
+		}
+		return !m_exit;
+	}
+
+	struct Item
+	{
+		Item(const std::string& shortcut, const std::string& description, const Command& command)
+			: shortcut(shortcut)
+			, description(description)
+			, command(command)
+		{
+		}
+
+		std::string shortcut;
+		std::string description;
+		Command command;
+	};
+	std::vector<Item> m_items;
+	bool m_exit = false;
+};

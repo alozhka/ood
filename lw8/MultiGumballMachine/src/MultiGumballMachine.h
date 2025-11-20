145da4b8064dbdf03f693b5925a3437cdcc1f53a
Sergey Lozhkin <alozhka@yahoo.com>
2025-11-21 00:53:07 +0300

    tests: написал тесты к обоим автоматам

--- lw8/MultiGumballMachine/src/MultiGumballMachine.h
+++ lw8/MultiGumballMachine/src/MultiGumballMachine.h
@@ -5,355 +5,355 @@
 
 struct IMultiGumballMachine
 {
-    virtual void ReleaseBall() = 0;
-    virtual unsigned GetBallCount() const = 0;
-    virtual unsigned GetQuarterCount() const = 0;
-    virtual void AddQuarter() = 0;
-    virtual void ReturnQuarters() = 0;
-    virtual unsigned RemoveQuarter() = 0;
-    virtual bool HasQuarters() const = 0;
-    virtual bool IsFullOfQuarters() const = 0;
+	virtual void ReleaseBall() = 0;
+	virtual unsigned GetBallCount() const = 0;
+	virtual unsigned GetQuarterCount() const = 0;
+	virtual void AddQuarter() = 0;
+	virtual void ReturnQuarters() = 0;
+	virtual unsigned RemoveQuarter() = 0;
+	virtual bool HasQuarters() const = 0;
+	virtual bool IsFullOfQuarters() const = 0;
 
-    virtual void SetSoldOutState() = 0;
-    virtual void SetNoQuarterState() = 0;
-    virtual void SetSoldState() = 0;
-    virtual void SetHasQuarterState() = 0;
+	virtual void SetSoldOutState() = 0;
+	virtual void SetNoQuarterState() = 0;
+	virtual void SetSoldState() = 0;
+	virtual void SetHasQuarterState() = 0;
 
-    virtual ~IMultiGumballMachine() = default;
+	virtual ~IMultiGumballMachine() = default;
 };
 
 struct IState
 {
-    virtual void InsertQuarter() = 0;
-    virtual void EjectQuarter() = 0;
-    virtual void TurnCrank() = 0;
-    virtual void Dispense() = 0;
-    virtual std::string ToString() const = 0;
-    virtual ~IState() = default;
+	virtual void InsertQuarter() = 0;
+	virtual void EjectQuarter() = 0;
+	virtual void TurnCrank() = 0;
+	virtual void Dispense() = 0;
+	virtual std::string ToString() const = 0;
+	virtual ~IState() = default;
 };
 
 class SoldState : public IState
 {
 public:
-    SoldState(IMultiGumballMachine& gumballMachine, std::ostream& out)
-        : m_gumballMachine(gumballMachine)
-        , out(out)
-    {
-    }
+	SoldState(IMultiGumballMachine& gumballMachine, std::ostream& out)
+		: m_gumballMachine(gumballMachine)
+		, out(out)
+	{
+	}
 
-    void InsertQuarter() override
-    {
-        out << "Please wait, we're already giving you a gumball\n";
-    }
+	void InsertQuarter() override
+	{
+		out << "Please wait, we're already giving you a gumball\n";
+	}
 
-    void EjectQuarter() override
-    {
-        out << "Sorry you already turned the crank\n";
-    }
+	void EjectQuarter() override
+	{
+		out << "Sorry you already turned the crank\n";
+	}
 
-    void TurnCrank() override
-    {
-        out << "Turning twice doesn't get you another gumball\n";
-    }
+	void TurnCrank() override
+	{
+		out << "Turning twice doesn't get you another gumball\n";
+	}
 
-    void Dispense() override
-    {
-        m_gumballMachine.ReleaseBall();
-        m_gumballMachine.RemoveQuarter();
+	void Dispense() override
+	{
+		m_gumballMachine.ReleaseBall();
+		m_gumballMachine.RemoveQuarter();
 
-        if (m_gumballMachine.GetBallCount() == 0)
-        {
-            out << "Oops, out of gumballs\n";
-            m_gumballMachine.SetSoldOutState();
-        }
-        else if (m_gumballMachine.HasQuarters())
-        {
-            m_gumballMachine.SetHasQuarterState();
-        }
-        else
-        {
-            m_gumballMachine.SetNoQuarterState();
-        }
-    }
+		if (m_gumballMachine.GetBallCount() == 0)
+		{
+			out << "Oops, out of gumballs\n";
+			m_gumballMachine.SetSoldOutState();
+		}
+		else if (m_gumballMachine.HasQuarters())
+		{
+			m_gumballMachine.SetHasQuarterState();
+		}
+		else
+		{
+			m_gumballMachine.SetNoQuarterState();
+		}
+	}
 
-    std::string ToString() const override
-    {
-        return "delivering a gumball";
-    }
+	std::string ToString() const override
+	{
+		return "delivering a gumball";
+	}
 
 private:
-    IMultiGumballMachine& m_gumballMachine;
-    std::ostream& out;
+	IMultiGumballMachine& m_gumballMachine;
+	std::ostream& out;
 };
 
 class SoldOutState : public IState
 {
 public:
-    SoldOutState(IMultiGumballMachine& gumballMachine, std::ostream& out)
-        : m_gumballMachine(gumballMachine)
-        , out(out)
-    {
-    }
+	SoldOutState(IMultiGumballMachine& gumballMachine, std::ostream& out)
+		: m_gumballMachine(gumballMachine)
+		, out(out)
+	{
+	}
 
-    void InsertQuarter() override
-    {
-        out << "You can't insert a quarter, the machine is sold out\n";
-    }
+	void InsertQuarter() override
+	{
+		out << "You can't insert a quarter, the machine is sold out\n";
+	}
 
-    void EjectQuarter() override
-    {
-        if (m_gumballMachine.HasQuarters())
-        {
-            m_gumballMachine.ReturnQuarters();
-        }
-        else
-        {
-            out << "You can't eject, you haven't inserted a quarter yet\n";
-        }
-    }
+	void EjectQuarter() override
+	{
+		if (m_gumballMachine.HasQuarters())
+		{
+			m_gumballMachine.ReturnQuarters();
+		}
+		else
+		{
+			out << "You can't eject, you haven't inserted a quarter yet\n";
+		}
+	}
 
-    void TurnCrank() override
-    {
-        out << "You turned but there's no gumballs\n";
-    }
+	void TurnCrank() override
+	{
+		out << "You turned but there's no gumballs\n";
+	}
 
-    void Dispense() override
-    {
-        out << "No gumball dispensed\n";
-    }
+	void Dispense() override
+	{
+		out << "No gumball dispensed\n";
+	}
 
-    std::string ToString() const override
-    {
-        return "sold out";
-    }
+	std::string ToString() const override
+	{
+		return "sold out";
+	}
 
 private:
-    IMultiGumballMachine& m_gumballMachine;
-    std::ostream& out;
+	IMultiGumballMachine& m_gumballMachine;
+	std::ostream& out;
 };
 
 class HasQuarterState : public IState
 {
 public:
-    explicit HasQuarterState(IMultiGumballMachine& gumballMachine, std::ostream& out)
-        : m_gumballMachine(gumballMachine)
-        , out(out)
-    {
-    }
+	explicit HasQuarterState(IMultiGumballMachine& gumballMachine, std::ostream& out)
+		: m_gumballMachine(gumballMachine)
+		, out(out)
+	{
+	}
 
-    void InsertQuarter() override
-    {
-        if (m_gumballMachine.IsFullOfQuarters())
-        {
-            out << "You can't insert another quarter, the machine is full\n";
-        }
-        else
-        {
-            out << "You inserted another quarter\n";
-            m_gumballMachine.AddQuarter();
-        }
-    }
+	void InsertQuarter() override
+	{
+		if (m_gumballMachine.IsFullOfQuarters())
+		{
+			out << "You can't insert another quarter, the machine is full\n";
+		}
+		else
+		{
+			out << "You inserted another quarter\n";
+			m_gumballMachine.AddQuarter();
+		}
+	}
 
-    void EjectQuarter() override
-    {
-        m_gumballMachine.ReturnQuarters();
-        m_gumballMachine.SetNoQuarterState();
-    }
+	void EjectQuarter() override
+	{
+		m_gumballMachine.ReturnQuarters();
+		m_gumballMachine.SetNoQuarterState();
+	}
 
-    void TurnCrank() override
-    {
-        out << "You turned...\n";
-        m_gumballMachine.SetSoldState();
-    }
+	void TurnCrank() override
+	{
+		out << "You turned...\n";
+		m_gumballMachine.SetSoldState();
+	}
 
-    void Dispense() override
-    {
-        out << "No gumball dispensed\n";
-    }
+	void Dispense() override
+	{
+		out << "No gumball dispensed\n";
+	}
 
-    std::string ToString() const override
-    {
-        return "waiting for turn of crank";
-    }
+	std::string ToString() const override
+	{
+		return "waiting for turn of crank";
+	}
 
 private:
-    IMultiGumballMachine& m_gumballMachine;
-    std::ostream& out;
+	IMultiGumballMachine& m_gumballMachine;
+	std::ostream& out;
 };
 
 class NoQuarterState : public IState
 {
 public:
-    explicit NoQuarterState(IMultiGumballMachine& gumballMachine, std::ostream& out)
-        : m_gumballMachine(gumballMachine)
-        , out(out)
-    {
-    }
+	explicit NoQuarterState(IMultiGumballMachine& gumballMachine, std::ostream& out)
+		: m_gumballMachine(gumballMachine)
+		, out(out)
+	{
+	}
 
-    void InsertQuarter() override
-    {
-        out << "You inserted a quarter\n";
-        m_gumballMachine.AddQuarter();
-        m_gumballMachine.SetHasQuarterState();
-    }
+	void InsertQuarter() override
+	{
+		out << "You inserted a quarter\n";
+		m_gumballMachine.AddQuarter();
+		m_gumballMachine.SetHasQuarterState();
+	}
 
-    void EjectQuarter() override
-    {
-        out << "You haven't inserted a quarter\n";
-    }
+	void EjectQuarter() override
+	{
+		out << "You haven't inserted a quarter\n";
+	}
 
-    void TurnCrank() override
-    {
-        out << "You turned but there's no quarter\n";
-    }
+	void TurnCrank() override
+	{
+		out << "You turned but there's no quarter\n";
+	}
 
-    void Dispense() override
-    {
-        out << "You need to pay first\n";
-    }
+	void Dispense() override
+	{
+		out << "You need to pay first\n";
+	}
 
-    std::string ToString() const override
-    {
-        return "waiting for quarter";
-    }
+	std::string ToString() const override
+	{
+		return "waiting for quarter";
+	}
 
 private:
-    IMultiGumballMachine& m_gumballMachine;
-    std::ostream& out;
+	IMultiGumballMachine& m_gumballMachine;
+	std::ostream& out;
 };
 
 class MultiGumballMachine : private IMultiGumballMachine
 {
 public:
-    static constexpr unsigned MAX_QUARTERS = 5;
+	static constexpr unsigned MAX_QUARTERS = 5;
 
-    explicit MultiGumballMachine(unsigned numBalls, std::ostream& out)
-        : m_count(numBalls)
+	explicit MultiGumballMachine(unsigned numBalls, std::ostream& out)
+		: m_count(numBalls)
 		, m_soldState(*this, out)
-        , m_soldOutState(*this, out)
-        , m_noQuarterState(*this, out)
-        , m_hasQuarterState(*this, out)
-        , m_state(&m_soldOutState)
-        , m_output(out)
-    {
-        if (m_count > 0)
-        {
-            m_state = &m_noQuarterState;
-        }
-    }
+		, m_soldOutState(*this, out)
+		, m_noQuarterState(*this, out)
+		, m_hasQuarterState(*this, out)
+		, m_state(&m_soldOutState)
+		, m_output(out)
+	{
+		if (m_count > 0)
+		{
+			m_state = &m_noQuarterState;
+		}
+	}
 
-    void EjectQuarter()
-    {
-        m_state->EjectQuarter();
-    }
+	void EjectQuarter()
+	{
+		m_state->EjectQuarter();
+	}
 
-    void InsertQuarter()
-    {
-        m_state->InsertQuarter();
-    }
+	void InsertQuarter()
+	{
+		m_state->InsertQuarter();
+	}
 
-    void TurnCrank()
-    {
-        m_state->TurnCrank();
-        m_state->Dispense();
-    }
+	void TurnCrank()
+	{
+		m_state->TurnCrank();
+		m_state->Dispense();
+	}
 
-    std::string ToString() const
-    {
-        return std::format("Mighty Gumball, Inc.\n"
-                          "C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
-                          "Inventory: {} gumball{}\n"
-                          "Quarters inserted: {}/{}\n"
-                          "Machine is {}",
-            m_count,
-            m_count != 1 ? "s" : "",
-            m_quarters,
-            MAX_QUARTERS,
-            m_state->ToString());
-    }
+	std::string ToString() const
+	{
+		return std::format("Mighty Gumball, Inc.\n"
+						   "C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
+						   "Inventory: {} gumball{}\n"
+						   "Quarters inserted: {}/{}\n"
+						   "Machine is {}",
+			m_count,
+			m_count != 1 ? "s" : "",
+			m_quarters,
+			MAX_QUARTERS,
+			m_state->ToString());
+	}
 
 private:
-    unsigned GetBallCount() const override
-    {
-        return m_count;
-    }
+	unsigned GetBallCount() const override
+	{
+		return m_count;
+	}
 
-    unsigned GetQuarterCount() const override
-    {
-        return m_quarters;
-    }
+	unsigned GetQuarterCount() const override
+	{
+		return m_quarters;
+	}
 
-    void AddQuarter() override
-    {
-        if (m_quarters < MAX_QUARTERS)
-        {
-            ++m_quarters;
-        }
-    }
+	void AddQuarter() override
+	{
+		if (m_quarters < MAX_QUARTERS)
+		{
+			++m_quarters;
+		}
+	}
 
-    void ReturnQuarters() override
-    {
-        if (m_quarters > 0)
-        {
-            m_output << "Returning " << m_quarters << " quarter" << (m_quarters != 1 ? "s" : "") << "\n";
-            m_quarters = 0;
-        }
-    }
+	void ReturnQuarters() override
+	{
+		if (m_quarters > 0)
+		{
+			m_output << "Returning " << m_quarters << " quarter" << (m_quarters != 1 ? "s" : "") << "\n";
+			m_quarters = 0;
+		}
+	}
 
-    unsigned RemoveQuarter() override
-    {
-        if (m_quarters > 0)
-        {
-            return --m_quarters;
-        }
-        return 0;
-    }
+	unsigned RemoveQuarter() override
+	{
+		if (m_quarters > 0)
+		{
+			return --m_quarters;
+		}
+		return 0;
+	}
 
-    bool HasQuarters() const override
-    {
-        return m_quarters > 0;
-    }
+	bool HasQuarters() const override
+	{
+		return m_quarters > 0;
+	}
 
-    bool IsFullOfQuarters() const override
-    {
-        return m_quarters >= MAX_QUARTERS;
-    }
+	bool IsFullOfQuarters() const override
+	{
+		return m_quarters >= MAX_QUARTERS;
+	}
 
-    void ReleaseBall() override
-    {
-        if (m_count != 0)
-        {
-            m_output << "A gumball comes rolling out the slot...\n";
-            --m_count;
-        }
-    }
+	void ReleaseBall() override
+	{
+		if (m_count != 0)
+		{
+			m_output << "A gumball comes rolling out the slot...\n";
+			--m_count;
+		}
+	}
 
-    void SetSoldOutState() override
-    {
-        m_state = &m_soldOutState;
-    }
+	void SetSoldOutState() override
+	{
+		m_state = &m_soldOutState;
+	}
 
-    void SetNoQuarterState() override
-    {
-        m_state = &m_noQuarterState;
-    }
+	void SetNoQuarterState() override
+	{
+		m_state = &m_noQuarterState;
+	}
 
-    void SetSoldState() override
-    {
-        m_state = &m_soldState;
-    }
+	void SetSoldState() override
+	{
+		m_state = &m_soldState;
+	}
 
-    void SetHasQuarterState() override
-    {
-        m_state = &m_hasQuarterState;
-    }
+	void SetHasQuarterState() override
+	{
+		m_state = &m_hasQuarterState;
+	}
 
-    unsigned m_count = 0;
-    unsigned m_quarters = 0;
-    SoldState m_soldState;
-    SoldOutState m_soldOutState;
-    NoQuarterState m_noQuarterState;
-    HasQuarterState m_hasQuarterState;
-    IState* m_state;
-    std::ostream& m_output;
+	unsigned m_count = 0;
+	unsigned m_quarters = 0;
+	SoldState m_soldState;
+	SoldOutState m_soldOutState;
+	NoQuarterState m_noQuarterState;
+	HasQuarterState m_hasQuarterState;
+	IState* m_state;
+	std::ostream& m_output;
 };
---
+++ lw8/MultiGumballMachine/tests/GumballMachineTests.cpp
@@ -0,0 +1,359 @@
+#include "../src/MultiGumballMachine.h"
+#include "../src/NaiveMultiGumballMachine.h"
+#include "gtest/gtest.h"
+#include <sstream>
+
+// Сравнительные тесты - проверяем что обе реализации ведут себя одинаково
+class MultiGumballMachineComparisonTests : public testing::Test
+{
+protected:
+	void SetUp() override
+	{
+		naiveOutput = std::make_unique<std::ostringstream>();
+		stateOutput = std::make_unique<std::ostringstream>();
+		naiveMachine = std::make_unique<NaiveMultiGumballMachine>(3, *naiveOutput);
+		stateMachine = std::make_unique<MultiGumballMachine>(3, *stateOutput);
+	}
+
+	void ClearOutputs()
+	{
+		naiveOutput->str("");
+		stateOutput->str("");
+	}
+
+	std::unique_ptr<std::ostringstream> naiveOutput, stateOutput;
+	std::unique_ptr<NaiveMultiGumballMachine> naiveMachine;
+	std::unique_ptr<MultiGumballMachine> stateMachine;
+};
+
+TEST_F(MultiGumballMachineComparisonTests, InitialState)
+{
+	std::string naiveString = naiveMachine->ToString();
+	std::string stateString = stateMachine->ToString();
+
+	EXPECT_EQ(naiveString, stateString);
+	EXPECT_EQ(naiveString,
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
+		"Inventory: 3 gumballs\n"
+		"Quarters inserted: 0/5\n"
+		"Machine is waiting for quarter");
+}
+
+TEST_F(MultiGumballMachineComparisonTests, InsertSingleQuarter)
+{
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+
+	EXPECT_EQ(naiveOutput->str(), stateOutput->str());
+	EXPECT_EQ(naiveOutput->str(), "You inserted a quarter\n");
+
+	std::string naiveString = naiveMachine->ToString();
+	std::string stateString = stateMachine->ToString();
+	EXPECT_EQ(naiveString, stateString);
+	EXPECT_EQ(naiveString,
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
+		"Inventory: 3 gumballs\n"
+		"Quarters inserted: 1/5\n"
+		"Machine is waiting for turn of crank");
+}
+
+TEST_F(MultiGumballMachineComparisonTests, InsertMultipleQuarters)
+{
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+
+	ClearOutputs();
+
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+
+	EXPECT_EQ(naiveOutput->str(), stateOutput->str());
+	EXPECT_EQ(naiveOutput->str(), "You inserted another quarter\n");
+
+	std::string naiveString = naiveMachine->ToString();
+	std::string stateString = stateMachine->ToString();
+	EXPECT_EQ(naiveString, stateString);
+	EXPECT_EQ(naiveString,
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
+		"Inventory: 3 gumballs\n"
+		"Quarters inserted: 2/5\n"
+		"Machine is waiting for turn of crank");
+}
+
+TEST_F(MultiGumballMachineComparisonTests, HasMaxQuarterLimit)
+{
+	for (int i = 0; i < 5; ++i)
+	{
+		naiveMachine->InsertQuarter();
+		stateMachine->InsertQuarter();
+	}
+
+	std::string naiveString = naiveMachine->ToString();
+	std::string stateString = stateMachine->ToString();
+	EXPECT_EQ(naiveString, stateString);
+	EXPECT_EQ(naiveString,
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
+		"Inventory: 3 gumballs\n"
+		"Quarters inserted: 5/5\n"
+		"Machine is waiting for turn of crank");
+
+	ClearOutputs();
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+
+	EXPECT_EQ(naiveOutput->str(), stateOutput->str());
+	EXPECT_EQ(naiveOutput->str(), "You can't insert another quarter, the machine is full\n");
+}
+
+TEST_F(MultiGumballMachineComparisonTests, ReturnsLeftMoney)
+{
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+
+	ClearOutputs();
+	naiveMachine->EjectQuarter();
+	stateMachine->EjectQuarter();
+
+	EXPECT_EQ(naiveOutput->str(), stateOutput->str());
+	EXPECT_EQ(naiveOutput->str(), "Returning 2 quarters\n");
+
+	std::string naiveString = naiveMachine->ToString();
+	std::string stateString = stateMachine->ToString();
+	EXPECT_EQ(naiveString, stateString);
+	EXPECT_EQ(naiveString,
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
+		"Inventory: 3 gumballs\n"
+		"Quarters inserted: 0/5\n"
+		"Machine is waiting for quarter");
+}
+
+TEST_F(MultiGumballMachineComparisonTests, TurnCrankWithQuarters)
+{
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+
+	ClearOutputs();
+	naiveMachine->TurnCrank();
+	stateMachine->TurnCrank();
+
+	EXPECT_EQ(naiveOutput->str(), stateOutput->str());
+	EXPECT_EQ(naiveOutput->str(),
+		"You turned...\n"
+		"A gumball comes rolling out the slot...\n");
+
+	std::string naiveString = naiveMachine->ToString();
+	std::string stateString = stateMachine->ToString();
+	EXPECT_EQ(naiveString, stateString);
+	EXPECT_EQ(naiveString,
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
+		"Inventory: 2 gumballs\n"
+		"Quarters inserted: 0/5\n"
+		"Machine is waiting for quarter");
+}
+
+TEST_F(MultiGumballMachineComparisonTests, MultipleTurnsWithMultipleQuarters)
+{
+	naiveMachine->InsertQuarter();
+	naiveMachine->InsertQuarter();
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+	ClearOutputs();
+
+	naiveMachine->TurnCrank();
+	stateMachine->TurnCrank();
+	naiveMachine->TurnCrank();
+	stateMachine->TurnCrank();
+
+	EXPECT_EQ(naiveOutput->str(), stateOutput->str());
+	EXPECT_EQ(naiveOutput->str(),
+		"You turned...\n"
+		"A gumball comes rolling out the slot...\n"
+		"You turned...\n"
+		"A gumball comes rolling out the slot...\n");
+
+	std::string naiveString = naiveMachine->ToString();
+	std::string stateString = stateMachine->ToString();
+	EXPECT_EQ(naiveString, stateString);
+	EXPECT_EQ(naiveString,
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
+		"Inventory: 1 gumball\n"
+		"Quarters inserted: 1/5\n"
+		"Machine is waiting for turn of crank");
+}
+
+TEST_F(MultiGumballMachineComparisonTests, MoreQuartersThanGumballs)
+{
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+
+	ClearOutputs();
+	naiveMachine->TurnCrank();
+	naiveMachine->TurnCrank();
+	naiveMachine->TurnCrank();
+	stateMachine->TurnCrank();
+	stateMachine->TurnCrank();
+	stateMachine->TurnCrank();
+
+	EXPECT_EQ(naiveOutput->str(), stateOutput->str());
+	EXPECT_EQ(naiveOutput->str(),
+		"You turned...\n"
+		"A gumball comes rolling out the slot...\n"
+		"You turned...\n"
+		"A gumball comes rolling out the slot...\n"
+		"You turned...\n"
+		"A gumball comes rolling out the slot...\n"
+		"Oops, out of gumballs\n");
+
+	std::string naiveString = naiveMachine->ToString();
+	std::string stateString = stateMachine->ToString();
+	EXPECT_EQ(naiveString, stateString);
+	EXPECT_EQ(naiveString,
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
+		"Inventory: 0 gumballs\n"
+		"Quarters inserted: 1/5\n"
+		"Machine is sold out");
+
+	ClearOutputs();
+	naiveMachine->EjectQuarter();
+	stateMachine->EjectQuarter();
+
+	EXPECT_EQ(naiveOutput->str(), stateOutput->str());
+	EXPECT_EQ(naiveOutput->str(), "Returning 1 quarter\n");
+}
+
+TEST_F(MultiGumballMachineComparisonTests, SoldOutState)
+{
+	std::ostringstream naiveEmptyOutput, stateEmptyOutput;
+	NaiveMultiGumballMachine naiveEmpty(0, naiveEmptyOutput);
+	MultiGumballMachine stateEmpty(0, stateEmptyOutput);
+
+	std::string naiveString = naiveEmpty.ToString();
+	std::string stateString = stateEmpty.ToString();
+	EXPECT_EQ(naiveString, stateString);
+	EXPECT_EQ(naiveString,
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
+		"Inventory: 0 gumballs\n"
+		"Quarters inserted: 0/5\n"
+		"Machine is sold out");
+
+	naiveEmptyOutput.str("");
+	stateEmptyOutput.str("");
+	naiveEmpty.InsertQuarter();
+	stateEmpty.InsertQuarter();
+
+	EXPECT_EQ(naiveEmptyOutput.str(), stateEmptyOutput.str());
+	EXPECT_EQ(naiveEmptyOutput.str(), "You can't insert a quarter, the machine is sold out\n");
+}
+
+TEST_F(MultiGumballMachineComparisonTests, EjectFromSoldOutWithQuarters)
+{
+	std::ostringstream naiveSpecialOutput, stateSpecialOutput;
+	NaiveMultiGumballMachine naiveSpecial(1, naiveSpecialOutput);
+	MultiGumballMachine stateSpecial(1, stateSpecialOutput);
+
+	naiveSpecial.InsertQuarter();
+	stateSpecial.InsertQuarter();
+	naiveSpecial.InsertQuarter();
+	stateSpecial.InsertQuarter();
+	naiveSpecial.InsertQuarter();
+	stateSpecial.InsertQuarter();
+	naiveSpecial.TurnCrank();
+	stateSpecial.TurnCrank();
+
+	std::string naiveString = naiveSpecial.ToString();
+	std::string stateString = stateSpecial.ToString();
+	EXPECT_EQ(naiveString, stateString);
+	EXPECT_EQ(naiveString,
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
+		"Inventory: 0 gumballs\n"
+		"Quarters inserted: 2/5\n"
+		"Machine is sold out");
+
+	naiveSpecialOutput.str("");
+	stateSpecialOutput.str("");
+	naiveSpecial.EjectQuarter();
+	stateSpecial.EjectQuarter();
+
+	EXPECT_EQ(naiveSpecialOutput.str(), stateSpecialOutput.str());
+	EXPECT_EQ(naiveSpecialOutput.str(), "Returning 2 quarters\n");
+}
+
+TEST_F(MultiGumballMachineComparisonTests, TurnCrankWithoutQuarters)
+{
+	ClearOutputs();
+	naiveMachine->TurnCrank();
+	stateMachine->TurnCrank();
+
+	EXPECT_EQ(naiveOutput->str(), stateOutput->str());
+	EXPECT_EQ(naiveOutput->str(), "You turned but there's no quarter\n");
+}
+
+TEST_F(MultiGumballMachineComparisonTests, EjectWithoutQuarters)
+{
+	ClearOutputs();
+	naiveMachine->EjectQuarter();
+	stateMachine->EjectQuarter();
+
+	EXPECT_EQ(naiveOutput->str(), stateOutput->str());
+	EXPECT_EQ(naiveOutput->str(), "You haven't inserted a quarter\n");
+}
+
+TEST_F(MultiGumballMachineComparisonTests, ComplexUsageScenarioFullOutput)
+{
+	naiveMachine->InsertQuarter();
+	naiveMachine->InsertQuarter();
+	naiveMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+	stateMachine->InsertQuarter();
+
+	EXPECT_EQ(naiveOutput->str(), stateOutput->str());
+	EXPECT_EQ(naiveOutput->str(),
+		"You inserted a quarter\n"
+		"You inserted another quarter\n"
+		"You inserted another quarter\n");
+
+	ClearOutputs();
+
+	naiveMachine->TurnCrank();
+	naiveMachine->TurnCrank();
+	stateMachine->TurnCrank();
+	stateMachine->TurnCrank();
+
+	EXPECT_EQ(naiveOutput->str(), stateOutput->str());
+	EXPECT_EQ(naiveOutput->str(),
+		"You turned...\n"
+		"A gumball comes rolling out the slot...\n"
+		"You turned...\n"
+		"A gumball comes rolling out the slot...\n");
+
+	std::string naiveString = naiveMachine->ToString();
+	std::string stateString = stateMachine->ToString();
+	EXPECT_EQ(naiveString, stateString);
+	EXPECT_EQ(naiveString,
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016 (Multi-Quarter)\n"
+		"Inventory: 1 gumball\n"
+		"Quarters inserted: 1/5\n"
+		"Machine is waiting for turn of crank");
+}

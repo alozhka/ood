78e0b01f37774625c1461738e438abb71d950d2b
Sergey Lozhkin <alozhka@yahoo.com>
2025-11-20 20:31:18 +0300

    tests: написал тест на выдачу жвачки

--- lw8/GumballMachine/tests/GumballMachineTests.cpp
+++ lw8/GumballMachine/tests/GumballMachineTests.cpp
@@ -5,10 +5,10 @@ class GumballMachineTests : public testing::Test
 {
 protected:
 	std::ostringstream output{};
-	GumballMachine gumballMachine{ 4, output };
+	GumballMachine gumballMachine{ 2, output };
 };
 
-TEST_F(GumballMachineTests, NoQuarterState)
+TEST_F(GumballMachineTests, CanInsertQuarter)
 {
 	output << gumballMachine.ToString() << std::endl;
 	gumballMachine.TurnCrank();
@@ -19,7 +19,7 @@ TEST_F(GumballMachineTests, NoQuarterState)
 	EXPECT_EQ(
 		"Mighty Gumball, Inc.\n"
 		"C++-enabled Standing Gumball Model #2016\n"
-		"Inventory: 4 gumballs\n"
+		"Inventory: 2 gumballs\n"
 		"Machine is waiting for quarter\n"
 		"You turned but there's no quarter\n"
 		"You need to pay first\n"
@@ -27,7 +27,28 @@ TEST_F(GumballMachineTests, NoQuarterState)
 		"You inserted a quarter\n"
 		"Mighty Gumball, Inc.\n"
 		"C++-enabled Standing Gumball Model #2016\n"
-		"Inventory: 4 gumballs\n"
+		"Inventory: 2 gumballs\n"
 		"Machine is waiting for turn of crank\n",
+		output.str());
+}
+
+TEST_F(GumballMachineTests, ReceivesGumball)
+{
+	gumballMachine.InsertQuarter();
+	output << gumballMachine.ToString() << std::endl;
+	gumballMachine.TurnCrank();
+	output << gumballMachine.ToString() << std::endl;
+
+	EXPECT_EQ(
+		"You inserted a quarter\n"
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016\n"
+		"Inventory: 2 gumballs\n"
+		"Machine is waiting for turn of crank\n"
+		"You turned...\n"
+		"Mighty Gumball, Inc.\n"
+		"C++-enabled Standing Gumball Model #2016\n"
+		"Inventory: 1 gumball\n"
+		"Machine is waiting for quarter\n",
 		output.str());
 }
